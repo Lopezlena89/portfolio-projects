edd8a95234dec0889cdb2782c9e996f0388645fb
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-05-07 21:06:53 -0600

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
+
 import './globals.css'
 
 import { Providers } from '@/components/providers/Providers';
@@ -9,8 +9,35 @@ import { Providers } from '@/components/providers/Providers';
 export const metadata: Metadata = {
   title: 'Profile',
   description: 'Portfolio about my experience',
+  generator: 'Next.js',
   keywords:["portfolio","lumava","web developer"],
-  authors:[{ name: "Luis Mariano Lopez Lena Valdivieso", url: "https://lumava.dev" }]
+  authors:[{ name: "Luis Mariano Lopez Lena Valdivieso", url: "https://lumava.dev" }],
+  category:'Web services',
+  metadataBase:new URL("https://lumava.dev"),
+  alternates:{
+    canonical: '/',
+    languages: {
+      'es-MX': '/es-MX',
+    },
+  },
+  openGraph: {
+    title: 'Luis Mariano Lopez Lena Valdivieso',
+    description: 'Portfolio about my experience',
+    url: 'https://lumava.dev',
+    siteName: 'Luis Mariano Lopez Lena Valdivieso',
+    images: [
+      {
+        url: '/LOGO-03.webp', // Must be an absolute URL
+        width: 800,
+        height: 600,
+        alt:"Lumava Agency"
+      },
+      
+    ],
+    locale: 'es_MX',
+    type: 'website'
+  }
+
 }
 
 export default function RootLayout({
